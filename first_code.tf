7ef54d6941c7e0140574eeb3b225879a81c30a96
Adnan Ahmed Khan <iamkhanadnan@gmail.com>
2021-05-21 19:09:25 +0600

    Adds basic security group

--- first_code.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  profile = "default"
-  region = "us-west-2"
-}
-
-resourse "aws_s3_bucket" "tf_play" {
-  bucket = "tf_play_khanadnanxyz"
-  acl    = "private"
-}
---
+++ prod.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  profile = "default"
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "prod_tf_play" {
+  bucket = "tf-play-khanadnanxyz"
+  acl    = "private"
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Configuration standard http/https ports"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+
+}
