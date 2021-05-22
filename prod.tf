10990faafcee968dc793cb0b578b95728a398325
Adnan Ahmed Khan <iamkhanadnan@gmail.com>
2021-05-22 20:59:44 +0600

    Cleans the plan and Adds variables

--- prod.tf
+++ prod.tf
@@ -1,18 +1,36 @@
+variable "whitelist" {
+  type = list(string)
+}
+variable "web_image_id" {
+  type = string
+}
+variable "web_instance_type" {
+  type = string
+}
+variable "web_desired_capacity" {
+  type = number
+}
+variable "web_max_size" {
+  type = number
+}
+variable "web_min_size" {
+  type = number
+}
+
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
-resource "aws_s3_bucket" "prod_tf_play" {
-  bucket = "tf-play-khanadnanxyz"
+resource "aws_s3_bucket" "prod_tf_course" {
+  bucket = "tf-course-20191118"
   acl    = "private"
-}
-
-resource "aws_default_vpc" "default" {
   tags = {
-    Name = "Default VPC"
+    "Terraform" : "true"
   }
 }
+
+resource "aws_default_vpc" "default" {}
 
 resource "aws_default_subnet" "default_az1" {
   availability_zone = "us-west-2a"
@@ -30,22 +48,20 @@ resource "aws_default_subnet" "default_az2" {
 
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
-  description = "Configuration standard http/https ports"
+  description = "Allow standard http and https ports inbound and everything outbound"
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
-
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -58,81 +74,15 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-# resource "aws_instance" "prod_web" {
-#   count = 2
+module "web_app" {
+  source = "./modules/web_app"
 
-#   ami           = "ami-0715ec6d98dd151b5"
-#   instance_type = "t2.micro"
-
-#   vpc_security_group_ids = [
-#     aws_security_group.prod_web.id
-#   ]
-
-#   tags = {
-#     "Terraform" : "true"
-#   }
-# }
-
-# resource "aws_eip_association" "prod_web" {
-#   instance_id   = aws_instance.prod_web[0].id
-#   allocation_id = aws_eip.prod_web.id
-# }
-
-# resource "aws_eip" "prod_web" {
-#   tags = {
-#     "Terraform" : "true"
-#   }
-# }
-
-resource "aws_elb" "prod_web" {
-  name            = "prod-web-khanadnanxyz"
-  # instances       = aws_instance.prod_web[*].id
-  subnets         = [ aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id ]
-  security_groups = [ aws_security_group.prod_web.id ]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-     
-  }
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_launch_template" "prod_web" {
-  name_prefix   = "prod-web-khanadnanxyz"
-  image_id      = "ami-0715ec6d98dd151b5"
-  instance_type = "t2.micro"
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_autoscaling_group" "prod_web" {
-  vpc_zone_identifier = [ aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id ]
-  desired_capacity    = 1
-  max_size            = 1
-  min_size            = 1
-
-  launch_template {
-    id      = aws_launch_template.prod_web.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Terraform"
-    value               = "true"
-    propagate_at_launch = true
-
-  }
-}
-
-resource "aws_autoscaling_attachment" "prod_web" {
-  autoscaling_group_name = aws_autoscaling_group.prod_web.id
-  elb                    = aws_elb.prod_web.id
+  web_image_id         = var.web_image_id
+  web_instance_type    = var.web_instance_type
+  web_desired_capacity = var.web_desired_capacity
+  web_max_size         = var.web_max_size
+  web_min_size         = var.web_min_size
+  subnets              = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+  security_groups      = [aws_security_group.prod_web.id]
+  web_app	       = "prod"
 }
